9c4e49a610563145937686802c7402038340a853
Shawn M. Jones <smj@littleprojects.org>
2013-09-26 21:44:21 -0400

    likely final submission

--- assignment2/work/q2/q2-histogram.R
+++ assignment2/work/q2/q2-histogram.R
@@ -7,7 +7,7 @@ Mementos = d[,1]
 brk <- seq(0, 22157, 1)
 
 png("q2-histogram1.png")
-hist(Mementos, main = "URIs vs. Number of Mementos", breaks=brk, freq = T, xlab="Mementos", ylab="URIs")
+hist(Mementos, col=heat.colors(22157), main = "URIs vs. Number of Mementos", breaks=brk, freq = T, xlab="Mementos", ylab="URIs")
 
 dev.off()
 
@@ -17,7 +17,7 @@ Mementos = Mementos[which(Mementos<10543)]
 brk <- seq(0, 4000, 1)
 
 png("q2-histogram2.png")
-hist(Mementos, main = "URIs vs. Number of Mementos", breaks=brk, freq = T, xlab="Mementos", ylab="URIs")
+hist(Mementos, col=heat.colors(4000), main = "URIs vs. Number of Mementos", breaks=brk, freq = T, xlab="Mementos", ylab="URIs")
 
 dev.off()
 
@@ -26,6 +26,6 @@ Mementos = Mementos[which(Mementos<100)]
 brk <- seq(0, 100, 1)
 
 png("q2-histogram3.png")
-hist(Mementos, main = "URIs vs. Number of Mementos", breaks=brk, freq = T, xlab="Mementos", ylab="URIs")
+hist(Mementos, col=heat.colors(100), main = "URIs vs. Number of Mementos", breaks=brk, freq = T, xlab="Mementos", ylab="URIs")
 
 dev.off()
--- assignment2/work/q3/q3-scatterplot.R
+++ assignment2/work/q3/q3-scatterplot.R
@@ -6,8 +6,6 @@ data = d[,c(2,3)]
 
 png("q3-scatterplot.png")
 
-plot(data, ylab="Age (in days)", xlab="Number of Mementos", main = "Number of Mementos vs. Age of URI")
-
-# abline(lm(data[1]~data[2]), col="red")
+plot(data, col=c("blue"), ylab="Age (in days)", xlab="Number of Mementos", main = "Number of Mementos vs. Age of URI")
 
 dev.off()
