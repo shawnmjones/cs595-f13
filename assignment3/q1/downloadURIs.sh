4c05325ae0351db17aeeb17a409910406edbb8f8
Shawn M. Jones <smj@littleprojects.org>
2013-09-30 14:23:32 -0400

    all 1000 URIs, downloaded

--- assignment3/q1/downloadURIs.sh
+++ assignment3/q1/downloadURIs.sh
@@ -6,6 +6,7 @@ set -e
 input=$1
 listfile=$2
 workingdir=$3
+failedfile=$4
 
 echo "Removing ${listfile}"
 if [ -f "${listfile}" ]; then
@@ -25,7 +26,14 @@ for line in `cat ${input}`; do
     echo "${line}    ${sha}" >> ${listfile}
 
     outfileraw="${sha}.raw"
+    set +e
     curl -A "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)" "${line}" >> "${workingdir}/${outfileraw}"
+    status=$?
+
+    if [ $status -ne 0 ]; then
+        echo ${line} >> ${failedfile}
+        echo ${line} failed to work, moving on
+    fi
 
     outfileproc="${sha}.processed"
     lynx -dump -force_html "${workingdir}/${outfileraw}" >> "${workingdir}/${outfileproc}"
