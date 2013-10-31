f11ece6cb331e91fec674c804d7a819443e04ad7
Shawn M. Jones <smj@littleprojects.org>
2013-10-31 00:51:51 -0400

    everything but the writeup

--- assignment6/R-code/community-functions.R
+++ assignment6/R-code/community-functions.R
@@ -1,27 +1,57 @@
-library(igraph)
-library(igraphdata)
-library(Matrix)
-library(lattice)
+library(igraph) # for graph functions
+library(igraphdata) # for karate club data
 
 data(karate)
 
 club <- karate
-threshold <- 2
+threshold <- 5
 
-while( clusters(k) $no < threshold ) {
-  keb <- edge.betweenness(club)
+# graph prior to the breakup
+plot.igraph(club, 
+  vertex.color="#339900",
+  vertex.frame.color="#000000",
+  vertex.shape="circle",
+  vertex.size=11,
+  vertex.label.color="#ffffff",
+  edge.color="black",
+  main="Karate Club Graph Prior to Breakup",
+  vertex.label.font=2,
+  layout=layout.kamada.kawai,
+  vertex.label.cex=1.2
+)
+
+# Algorithm below graciously provided by Corren McCoy
+# also described by University of Michigan
+while( clusters(club) $no < threshold ) {
+  # calculate betweenness of all edges
+  club.edge.betweenness <- edge.betweenness(club)
   
-  decreasing.betweenness <- order(keb, decreasing = TRUE)
+  # remove edge with highest betweenness
+  decreasing.betweenness <- order(club.edge.betweenness, decreasing = TRUE)
   
   # for fun, pick off the ones with the lowest betweenness,
-  # and watch the graph kick out one memeber at a time
+  # and watch the club kick out one memeber at a time
   #decreasing.betweenness <- order(decreasing.betweenness)
   
+  # get the one with the highest edge betweenness
   highest.betweenness <- decreasing.betweenness[-1]
   
+  # acquire the edge to delete
   edge.to.delete <- get.edge(club, highest.betweenness)
   
+  # get rid of it
   club <- delete.edges(club, E(club, P = edge.to.delete))
 }
 
-plot.igraph(k)
+plot.igraph(club, 
+  vertex.color="#ff0000",
+  vertex.frame.color="#000000",
+  vertex.shape="circle",
+  vertex.size=11,
+  vertex.label.color="#ffffff",
+  edge.color="black",
+  main="Karate Club Split, Predicted by Girvan-Newman Betweenness Clustering",
+  vertex.label.font=2,
+  layout=layout.kamada.kawai,
+  vertex.label.cex=1.2
+  )
