8dca099160db2d56d6983c792ed52348e3a5c127
Shawn M. Jones <smj@littleprojects.org>
2013-11-05 23:23:29 -0500

    created club split

---
+++ assignment7/q1/createClubSplit.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python3
+
+import sys
+import json
+import networkx
+from networkx.readwrite import json_graph
+
+
+if __name__ == "__main__":
+    inputfile = sys.argv[1]
+
+    f = open(inputfile)
+    inputdata = json.load(f)
+    f.close()
+
+    club = json_graph.node_link_graph(inputdata)
+
+    # 4. repeate until we have 2 clusters
+    while (networkx.number_connected_components(club) < 2):
+        # 1. calculate edge-betweenness for all edges
+        # 3. recalculate betweenness
+        eb = networkx.edge_betweenness_centrality(club, weight='weight')
+    
+        # 2. remove the edge with highest betweenness
+        # Thanks Stack Overflow:
+        # http://stackoverflow.com/questions/16772071/sort-dict-by-value-python
+        edge2remove = sorted(eb.items(), key=lambda x:x[1], reverse=True)[0][0]
+    
+        club.remove_edge(*edge2remove)
+
+    # club should be split, sadly... :(
+
+    outputdata = json_graph.node_link_data(club)
+
+    print(json.dumps(outputdata, indent=4))
