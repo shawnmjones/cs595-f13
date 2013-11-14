61a6c8d03ce3e2247a44527a70bf54f29dedb2ab
Shawn M. Jones <smj@littleprojects.org>
2013-11-13 23:45:08 -0500

    beginnings of an answer to assignment 8

--- assignment8/q05/getFilmsLike.py
+++ assignment8/q05/getFilmsLike.py
@@ -10,11 +10,18 @@ import recommendations
 if __name__ == '__main__':
 
     film = sys.argv[1]
-    n = int(sys.argv[2])
+    threshold = int(sys.argv[2])
+    direction = sys.argv[3]
 
     prefs = recommendations.loadMovieLens('../data')
 
-    result = recommendations.calculateSimilarItems(prefs, n=n)
+    result = recommendations.calculateSimilarItems(prefs, n=1682)
 
-    print "Movie most like '" + film + "': '" + result[film][0][1]
-    print "Movie least like '" + film + "': '" + result[film][-1][1]
+    if direction == 'most':
+        print "Movies most like '" + film + "': '"
+        for i in range(0, threshold):
+            print result[film][i][1] + ' (' + str(result[film][i][0]) + ')'
+    else:
+        print "Movies least like '" + film + "': '"
+        for i in range(1, threshold):
+            print result[film][-i][1] + ' (' + str(result[film][-i][0]) + ')'
